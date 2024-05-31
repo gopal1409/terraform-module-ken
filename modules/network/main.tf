610d0cf48c7df35b982cf515db3ab8ca52fc5e4b
gopal <gopal1409@gmail.com>
2024-05-31 14:23:28 +0530

    terraform module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -18,14 +18,7 @@ resource "azurerm_subnet" "web-subnet" {
 #this subnet will give you a range of 255 ip address
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  #resource "nameoftheapi" "refrencelable"
-  #when you are creating a resource 
-  name     = "${local.resource_name_prefix}-${var.resource_group_name}"
-  ###sap-dev-rg-default
-  location = var.resource_group_location
-  tags = local.common_tags
-}
+
 
 
 locals {
